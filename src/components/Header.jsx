f8e7dc147d0debd07ba26690c467765f9e48170f
yukinagano <tawapeee16@gmail.com>
2019-08-12 10:51:43 -0700

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+export default class Header extends React.Component {
+    render(){
+        return(
+            <section id="header" className="header">
+            
+            </section>
+        )
+    }
+}
+
